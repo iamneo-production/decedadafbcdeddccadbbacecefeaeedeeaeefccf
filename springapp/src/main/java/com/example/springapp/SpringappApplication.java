a0650317f51ff2c9d19c5510954f7f289e2f3498
Iamneo <techadmin@iamneo.ai>
2023-08-17 06:52:34 +0000

    Backup Commit

--- springapp/src/main/java/com/example/springapp/SpringappApplication.java
+++ springapp/src/main/java/com/example/springapp/SpringappApplication.java
@@ -2,12 +2,16 @@ package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@R
 @SpringBootApplication
 public class SpringappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringappApplication.class, args);
+	}
+	public String Welcome()
+	{
+		return "Welcome String Boot!";
 	}
 
 }
